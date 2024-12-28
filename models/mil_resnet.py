278a66aef421c0982260e312aa63573a711a309b
Kha7734 <huynhsikha2003@gmail.com>
2024-12-28 11:41:06 +0700

    - Models Module Code

--- models/mil_resnet.py
+++ models/mil_resnet.py
@@ -3,25 +3,31 @@ import torch.nn as nn
 from torchvision import models
 from utils import attention as AttentionLayer, gaussian_process as GPModel
 
-class MILResNet18(nn.Module):
+class BaseModel(nn.Module):
     def __init__(self, params=None):
-        super(MILResNet18, self).__init__()
-
-        # Default parameters if none are provided
+        super(BaseModel, self).__init__()
         if params is None:
             params = {}
         self.CHANNELS = params.get('channels', 1)
+        self.NUM_CLASSES = params.get('num_classes', 1)
+        self.DROP_PROB = params.get('drop_prob', 0.4)
+        self.INDUCING_POINTS = params.get('inducing_points', 32)
 
         self.resnet = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
         self.resnet.conv1 = nn.Conv2d(in_channels=self.CHANNELS, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False)
         self.resnet.fc = nn.Identity()
 
-        self.attention = AttentionLayer.AttentionLayer(input_dim=512, hidden_dim=512)
-        self.classifier = nn.Linear(512 + 1, 1)
-        self.attention_classifier = nn.Linear(512, 1)
-        self.dropout = nn.Dropout(0.4)
+    def forward(self, bags):
+        pass
 
-        inducing_points = torch.randn(32, 512)
+class CNN_ATT_GP(BaseModel):
+    def __init__(self, params=None):
+        super(CNN_ATT_GP, self).__init__(params)
+        self.attention = AttentionLayer.AttentionLayer(input_dim=512, hidden_dim=512)
+        self.classifier = nn.Linear(512 + 1, self.NUM_CLASSES)
+        self.dropout = nn.Dropout(self.DROP_PROB)
+
+        inducing_points = torch.randn(self.INDUCING_POINTS, 512)
         self.gp_layer = GPModel.GPModel(inducing_points=inducing_points)
 
         # Store parameters from the dictionary
@@ -29,6 +35,7 @@ class MILResNet18(nn.Module):
         self.projection_hidden_dim = params.get('projection_hidden_dim', 256)
         self.projection_output_dim = params.get('projection_output_dim', 128)
         self.projection_input_dim = 513 if self.projection_location == 'after_gp' else 512
+
         self.projection_head = nn.Sequential(
             nn.Linear(self.projection_input_dim, self.projection_hidden_dim),
             nn.ReLU(),
@@ -70,6 +77,58 @@ class MILResNet18(nn.Module):
         combine_features = self.dropout(combine_features)
 
         outputs = torch.sigmoid(self.classifier(combine_features))
-        att_outputs = torch.sigmoid(self.attention_classifier(attended_features_reshaped))
 
-        return outputs, att_outputs, attended_weights, gp_output, projection_output
+        return outputs, attended_weights, gp_output, projection_output
+
+class CNN_GP_ATT(BaseModel):
+    def __init__(self, params=None):
+        super(CNN_GP_ATT, self).__init__(params)
+        self.attention = AttentionLayer.AttentionLayer(input_dim=512, hidden_dim=512)
+        self.classifier = nn.Linear(512 + 1, self.NUM_CLASSES)
+        self.dropout = nn.Dropout(self.DROP_PROB)
+
+        inducing_points = torch.randn(self.INDUCING_POINTS, 512)
+        self.gp_layer = GPModel.GPModel(inducing_points=inducing_points)
+
+        # Store parameters from the dictionary
+        self.projection_location = params.get('projection_location', 'after_resnet')
+        self.projection_hidden_dim = params.get('projection_hidden_dim', 256)
+        self.projection_output_dim = params.get('projection_output_dim', 128)
+        self.projection_input_dim = 513 if self.projection_location == 'after_gp' else 512
+
+        self.projection_head = nn.Sequential(
+            nn.Linear(self.projection_input_dim, self.projection_hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.projection_hidden_dim, self.projection_output_dim)
+        )
+
+    def forward(self, bags):
+        if self.CHANNELS == 1:
+            batch_size, num_instances, c, h, w = bags.size()
+        else:
+            batch_size, num_instances, h, w, c = bags.size()
+
+        bags_flattened = bags.view(batch_size * num_instances, c, h, w)
+
+        features = self.resnet(bags_flattened)
+        features = self.dropout(features)
+        features = features.view(batch_size, num_instances, -1)
+
+        gp_output = self.gp_layer(features.view(batch_size * num_instances, -1))
+        gp_mean = gp_output.mean.view(batch_size, -1)
+        combine_features = torch.cat((features.view(batch_size, -1), gp_mean), dim=1)
+
+        attended_features, attended_weights = self.attention(combine_features)
+        attended_features_reshaped = attended_features.view(batch_size, -1)
+        outputs = self.classifier(self.dropout(attended_features_reshaped))
+
+        if self.projection_location == 'after_resnet':
+            projection_output = self.projection_head(features.view(batch_size * num_instances, -1))
+        elif self.projection_location == 'after_attention':
+            projection_output = self.projection_head(outputs)
+        elif self.projection_location == 'after_gp':
+            projection_output = self.projection_head(combine_features)
+        else:
+            raise ValueError("Invalid projection location. Choose from 'after_resnet', 'after_attention', or 'after_gp'.")
+
+        return outputs, attended_weights, gp_output, projection_output
