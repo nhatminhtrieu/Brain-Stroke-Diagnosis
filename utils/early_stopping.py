998d8b39a7e6da1718dc70700e4f0a5935c4b124
Kha7734 <huynhsikha2003@gmail.com>
2025-01-14 16:19:42 +0700

    - result with redundancy data

--- utils/early_stopping.py
+++ utils/early_stopping.py
@@ -1,6 +1,6 @@
 import torch
 
-class EarlyStopping:
+class EarlyStoppingForOptimization:
     def __init__(self, patience=5, verbose=False, delta=0):
         """
         Args:
@@ -51,3 +51,36 @@ class EarlyStopping:
         if self.verbose:
             print(f'Validation accuracy increased ({self.best_score:.6f} --> {score:.6f}). Saving model ...')
         torch.save(model.state_dict(), 'checkpoint.pt')
+
+class EarlyStopping:
+    def __init__(self, patience=5, delta=0, verbose=False):
+        """
+        Args:
+            patience (int): How many epochs to wait after last time the validation metric improved.
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+            verbose (bool): If True, prints a message for each validation metric improvement.
+        """
+        self.patience = patience
+        self.delta = delta
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.best_model_state = None
+
+    def __call__(self, val_metric, model):
+        score = val_metric
+
+        if self.best_score is None:
+            self.best_score = score
+            self.best_model_state = model.state_dict()
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.best_model_state = model.state_dict()
+            self.counter = 0
